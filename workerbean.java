190349261b64217d86aedc6a74cc50fab718bf77
kasuboo <80378908+kasuboo@users.noreply.github.com>
2022-05-14 14:47:46 +0800

    Create workerbean.java

---
+++ workerbean.java
@@ -0,0 +1,24 @@
+package mybean.data;
+public class workerBean {
+	private String sitename;
+	private String workercode;
+	private String workername;
+	private String workersex;
+	private String workerbirthday;
+	private String workeraddress;
+	private String workerphone;
+	private String workerjob;
+	private String workerposition;
+	private String rigistertime;
+	public void setSitename(String site) {sitename=site;}
+	public String getSitename() {return sitename;}
+	public void setWorkercode(String code) {workercode=code;}
+	public String getWorkercode() {return workercode;}
+	public void setWorkername(String name) {workername=name;}
+	public String getWorkername() {return workername;}
+	public void setWorkerbirthday(String birth) {workerbirthday=birth;}
+	public String getWorkerbirthday() {return workerbirthday;}
+	public void setRigistertime(String time) {rigistertime=time;}
+	public String getRigistertime() {return rigistertime;}
+
+}
